58f5297811a6dbcc830641d32c6d35107f1f9a89
nitheeshcpu <nitishkoda@outlook.com>
2022-09-12 22:21:25 -0500

    fosifdoiajfoisadfjoasjdf

--- src/main/java/Graphics/GamePanel.java
+++ src/main/java/Graphics/GamePanel.java
@@ -23,15 +23,16 @@ public class GamePanel extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         backCard = new ImageIcon(GamePanel.class.getClassLoader().getResource("Cards/10.jpg"));
-        Image resized  = backCard.getImage().getScaledInstance(190,265,Image.SCALE_SMOOTH);
+        Image resized  = backCard.getImage().getScaledInstance(114,159,Image.SCALE_SMOOTH);
         back = new JButton(backCard);
         back.setLocation(300, 15);
-        back.setSize(190, 265);
+        back.setSize(114, 159);
         add(back);
 
         back.addActionListener(e -> {
             ImageIcon change = new ImageIcon(GamePanel.class.getClassLoader().getResource("Cards/9.png"));
-            back.setIcon(change);
+            Image resize  = change.getImage().getScaledInstance(114,159,Image.SCALE_SMOOTH);
+            back.setIcon(new ImageIcon(resize));
         });
 
 
